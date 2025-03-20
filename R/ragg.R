1d53b43cf9c045d05e536c48d0692d320eeb1c88
Simon Schulte <s.pedro@gmx.de>
2025-03-20 17:23:19 +0100

    added rlnorm on linear scale function

--- R/ragg.R
+++ R/ragg.R
@@ -10,10 +10,14 @@
 #' @export
 #'
 #' @examples
-ragg <- function(n, mean, sd = NULL, min = 0, max = Inf, meanlog = NULL, sdlog = NULL) {
-  if (!is.null(mean) & !is.null(sd) & min == -Inf & max == Inf) {
+ragg <- function(n, mean, sd = NULL, min = 0, max = Inf, meanlog = NULL, sdlog = NULL,
+                 log = FALSE) {
+  if (!is.null(mean) & !is.null(sd) & min == -Inf & max == Inf & isFALSE(log)) {
     # Normal distribution
     rnorm(n, mean, sd)
+  } else if (!is.null(mean) & !is.null(sd) & min == -Inf & max == Inf & isTRUE(log)) {
+    # Lognormal distribution
+    rlnorm2(n, mean, sd)
   } else if (!is.null(mean) & !is.null(sd)) {
     # Truncated normal
     rtruncnorm(n, a = min, b = max, mean = mean, sd = sd)
@@ -28,4 +32,23 @@ ragg <- function(n, mean, sd = NULL, min = 0, max = Inf, meanlog = NULL, sdlog =
   } else {
     stop('Case not implemented atm.')
   }
+}
+
+
+#' Random number generation for the lognormal distribution parameterised to the `mean` and `sd` on the **linear** scale.
+#' Taken from: https://stackoverflow.com/questions/56821688/sample-a-lognormal-distribution-to-an-exact-mean-and-sd
+#'
+#' @param n
+#' @param mean the mean on the linear scale
+#' @param sd the standard deviation on the linear scale
+#'
+#' @return
+#' @export
+#'
+#' @examples
+rlnorm2 <- function(n, mean, sd) {
+  m2 <-  log(mean^2 / sqrt(sd^2 + mean^2))
+  sd2 <- sqrt(log(1 + (sd^2 / mean^2)))
+  r <- c(scale(rnorm(n)))
+  return(exp(sd2*r+m2))
 }
