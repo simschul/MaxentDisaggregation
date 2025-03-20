c9ffa565b371a8ad4753a4b3b9a81fcdc0ea9db7
Simon Schulte <s.pedro@gmx.de>
2025-03-20 17:03:05 +0100

    added lognormal dist

--- R/ragg.R
+++ R/ragg.R
@@ -10,7 +10,7 @@
 #' @export
 #'
 #' @examples
-ragg <- function(n, mean, sd = NULL, min = 0, max = Inf) {
+ragg <- function(n, mean, sd = NULL, min = 0, max = Inf, meanlog = NULL, sdlog = NULL) {
   if (!is.null(mean) & !is.null(sd) & min == -Inf & max == Inf) {
     # Normal distribution
     rnorm(n, mean, sd)
@@ -23,6 +23,8 @@ ragg <- function(n, mean, sd = NULL, min = 0, max = Inf) {
   } else if (is.null(mean) & is.null(sd) & is.finite(min) & is.finite(max)) {
     # uniform
     runif(n, min = min, max = max)
+  } else if (is.null(mean) & is.null(sd) & !is.null(meanlog) & !is.null(sdlog)) {
+    rlnorm(n, meanlog = meanlog, sdlog = sdlog)
   } else {
     stop('Case not implemented atm.')
   }
